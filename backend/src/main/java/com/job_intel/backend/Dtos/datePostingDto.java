05146a8707cc262ddaca0bac364c7dbbfb1942e4
Robert Bray <rbray77777@gmail.com>
2026-06-21 17:19:35 +0000

    dto for job trends done

---
+++ backend/src/main/java/com/job_intel/backend/Dtos/datePostingDto.java
@@ -0,0 +1,18 @@
+package com.job_intel.backend.Dtos;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class datePostingDto {
+
+    private LocalDate postedDate;
+    private Integer jobPostingCount;
+}
