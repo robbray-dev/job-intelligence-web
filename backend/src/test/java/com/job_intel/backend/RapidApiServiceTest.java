39b709b42bd432e0f6f10756dc5a78d12d2c3916
robbray-dev <rbray77777@gmail.com>
2026-07-04 03:25:25 +0000

    test for the api service

---
+++ backend/src/test/java/com/job_intel/backend/RapidApiServiceTest.java
@@ -0,0 +1,25 @@
+package com.job_intel.backend;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.job_intel.backend.job_board_apis.RapidApiService;
+
+@SpringBootTest
+class RapidApiServiceTest {
+
+    @Autowired 
+    private RapidApiService apiService;
+
+    
+    @Test
+    void apiServiceTest(){
+
+        String key = apiService.getApiKey();
+        System.out.println(key);
+        assertNotNull(key);
+    }
+}
