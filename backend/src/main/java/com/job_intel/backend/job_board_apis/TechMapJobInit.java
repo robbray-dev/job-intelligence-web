234c8f0384431066c640061c6a3a07b63a909dc9
robbray-dev <rbray77777@gmail.com>
2026-07-06 22:25:47 +0000

    made the tech job stuff static

--- backend/src/main/java/com/job_intel/backend/job_board_apis/TechMapJobInit.java
+++ backend/src/main/java/com/job_intel/backend/job_board_apis/TechMapJobInit.java
@@ -17,33 +17,28 @@ import lombok.Getter;
 @Component
 public class TechMapJobInit {
 
-    //might get removed cuz i dont need it
-    private HttpRequest request;
-
-    private HttpResponse<String> res;
-
-    
-    private final String apiKey;
    
 
-    public TechMapJobInit(@Value("${rapid.api.key}") String apiKey){
-
-        this.apiKey = apiKey;
-         this.request = HttpRequest.newBuilder()
+    public static String getJobDataFromApi() {
+         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create("https://daily-international-job-postings.p.rapidapi.com/api/v2/jobs/search?format=json&countryCode=us&hasSalary=true&page=1"))
-            .header("x-rapidapi-key", apiKey)
+            .header("x-rapidapi-key", "API_KEY_WILL_GO_HERE")
             .header("x-rapidapi-host", "daily-international-job-postings.p.rapidapi.com")
             .header("Content-Type", "application/json")
             .method("GET", HttpRequest.BodyPublishers.noBody())
             .build();
-                       //try catch block
+            
+
         try{
-            this.res = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> res = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            return res.body();
         } catch (IOException e) {
             System.out.println("Something went wrong. IO exception");
         } catch (InterruptedException e) {
             System.out.println("Something went wrong. Interrupted exception");
         }
+        return null;
+
     }
 
 
