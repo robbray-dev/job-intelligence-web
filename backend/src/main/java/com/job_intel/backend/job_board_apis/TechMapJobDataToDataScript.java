ebc091204bcf661066d9a95614e60bebe9265976
robbray-dev <rbray77777@gmail.com>
2026-07-06 23:17:46 +0000

    making the script

---
+++ backend/src/main/java/com/job_intel/backend/job_board_apis/TechMapJobDataToDataScript.java
@@ -0,0 +1,8 @@
+package com.job_intel.backend.job_board_apis;
+
+public class TechMapJobDataToDataScript {
+
+    public static void mapJobDataToEntity(String jobDataBody) {
+        // TODO:
+    }
+}
--- backend/src/main/java/com/job_intel/backend/job_board_apis/TechMapJobInit.java
+++ backend/src/main/java/com/job_intel/backend/job_board_apis/TechMapJobInit.java
@@ -6,15 +6,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@Component
 public class TechMapJobInit {
 
    
