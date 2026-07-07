b0a6b6171dab783fc8e737507aee92a22833dedb
robbray-dev <rbray77777@gmail.com>
2026-07-07 22:21:57 +0000

    removed key printing check

--- backend/src/main/java/com/job_intel/backend/job_board_apis/TechMapJobInit.java
+++ backend/src/main/java/com/job_intel/backend/job_board_apis/TechMapJobInit.java
@@ -12,7 +12,6 @@ public class TechMapJobInit {
 
     public static String getJobDataFromApi() {
         String key = System.getenv("RAPID_API_KEY");
-        System.out.println(key);
          HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create("https://daily-international-job-postings.p.rapidapi.com/api/v2/jobs/search?format=json&countryCode=us&hasSalary=true&page=1"))
             .header("x-rapidapi-key", key)
