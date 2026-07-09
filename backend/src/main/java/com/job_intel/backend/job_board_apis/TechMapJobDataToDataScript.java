15635ab3d3459e068ea8c9234d60bc75f3993a32
robbray-dev <rbray77777@gmail.com>
2026-07-09 02:57:55 +0000

    got the json mapper working

--- backend/src/main/java/com/job_intel/backend/job_board_apis/TechMapJobDataToDataScript.java
+++ backend/src/main/java/com/job_intel/backend/job_board_apis/TechMapJobDataToDataScript.java
@@ -1,8 +1,15 @@
 package com.job_intel.backend.job_board_apis;
 
+import org.json.*;
+
 public class TechMapJobDataToDataScript {
 
     public static void mapJobDataToEntity(String jobDataBody) {
-        // TODO:
+        // TODO: print out actual jobs with the correct JSON lib syntax
+        // ie job 1, job 2, etc
+        String jsoString = jobDataBody;
+        JSONObject obj = new JSONObject(jsoString);
+        String api = obj.getString("api");
+        System.out.println(api);
     }
 }
