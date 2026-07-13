0f26afc3f436de08bda943342f8832d4d4f4da33
robbray-dev <rbray77777@gmail.com>
2026-07-13 00:44:40 +0000

    did company mapping

--- backend/src/main/java/com/job_intel/backend/job_board_apis/TechMapJobDataToDataScript.java
+++ backend/src/main/java/com/job_intel/backend/job_board_apis/TechMapJobDataToDataScript.java
@@ -3,6 +3,8 @@ package com.job_intel.backend.job_board_apis;
 import org.apache.tomcat.util.digester.SystemPropertySource;
 import org.json.*;
 
+import com.job_intel.backend.models.Company;
+
 public class TechMapJobDataToDataScript {
 
     public static void mapJobDataToEntity(String jobDataBody) {
@@ -17,7 +19,29 @@ public class TechMapJobDataToDataScript {
 
         for (int i = 0; i < arr.length(); i++) {
             String occupation = arr.getJSONObject(i).getString("occupation");
+            //get company and store it in a componay object and store it with its repo
+
+            String company = arr.getJSONObject(i).getString("company");
+            System.out.println(company);
+
+            if(companyDBentry(company) == "good") {
+                System.out.println("success");
+            } else {
+                System.out.println("false");
+            }
+            //pass this down and place it in a method // companyDBentry(company name) { Company companyFromJobData = new Company("company"), companyRepo.save(companyFromJobData)}
             System.out.println(occupation);
+        }
+    }
+
+    private static String companyDBentry(String companyName){
+        Company companyFromJobData = new Company();
+        companyFromJobData.setName(companyName);
+        
+        if(companyFromJobData.getName() == companyName){
+            return "good";
+        } else {
+            return "bad";
         }
     }
 }
