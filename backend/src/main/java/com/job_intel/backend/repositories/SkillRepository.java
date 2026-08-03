8299b8d3853df6556d78371bf233f860b8feb461
robbray-dev <rbray77777@gmail.com>
2026-08-03 02:40:30 +0000

    findByName for skill

--- backend/src/main/java/com/job_intel/backend/repositories/SkillRepository.java
+++ backend/src/main/java/com/job_intel/backend/repositories/SkillRepository.java
@@ -1,11 +1,16 @@
 package com.job_intel.backend.repositories;
 
 import com.job_intel.backend.models.Skill;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface SkillRepository extends JpaRepository<Skill, Long> {
 
     boolean existsByName(String name);
+
+    Optional<Skill> findByName(String name);
 
 }
