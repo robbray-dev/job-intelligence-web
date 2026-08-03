130f0d528bb1d6c309edcb5a963991db1c376d05
robbray-dev <rbray77777@gmail.com>
2026-08-03 02:47:35 +0000

    company findByName

--- backend/src/main/java/com/job_intel/backend/repositories/CompanyRepository.java
+++ backend/src/main/java/com/job_intel/backend/repositories/CompanyRepository.java
@@ -1,9 +1,16 @@
 package com.job_intel.backend.repositories;
 
 import com.job_intel.backend.models.Company;
+import com.job_intel.backend.models.Skill;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CompanyRepository extends JpaRepository<Company,Long> {
+public interface CompanyRepository extends JpaRepository<Company, Long> {
 
- boolean existsByName(String name);
+    boolean existsByName(String name);
+
+    Optional<Skill> findByName(String name);
+
 }
